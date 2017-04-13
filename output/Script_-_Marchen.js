54bc3203790b606749a5dcdfa0c970c5982b9bfa
Mathias Bynens <mathias@qiwi.be>
2017-04-13 09:04:01 +0200

    Use chunking in `buildString`
    Thanks to @anba for the suggestion!
    
    https://github.com/tc39/test262/pull/971#issuecomment-293653032

--- output/Script_-_Marchen.js
+++ output/Script_-_Marchen.js
@@ -13,11 +13,18 @@ features: [regexp-unicode-property-escapes]
 ---*/
 
 const buildString = ({ loneCodePoints, ranges }) => {
+	const CHUNK_SIZE = 10000;
 	let result = String.fromCodePoint(...loneCodePoints);
 	for (const [start, end] of ranges) {
-		for (let codePoint = start; codePoint <= end; codePoint++) {
-			result += String.fromCodePoint(codePoint);
+		const codePoints = [];
+		for (let length = 0, codePoint = start; codePoint <= end; codePoint++) {
+			codePoints[length++] = codePoint;
+			if (length === CHUNK_SIZE) {
+				result += String.fromCodePoint(...codePoints);
+				codePoints.length = length = 0;
+			}
 		}
+		result += String.fromCodePoint(...codePoints);
 	}
 	return result;
 };

85f5c7ff3a2eaf7efb4b11c34b1329185e871d9a
Mathias Bynens <mathias@qiwi.be>
2021-12-23 14:00:19 +0100

    Generate tests for properties of strings
    This patch generates exhaustive tests that verifies every supported property of strings matches every single string it needs to. We also generate some similar-looking butdifferent strings and ensure those aren’t matched.
    
    Proposal: https://github.com/tc39/proposal-regexp-set-notation

--- harness/regExpUtils.js
+++ harness/regExpUtils.js
@@ -15,17 +15,52 @@ function buildString({ loneCodePoints, ranges }) {
   return result;
 }
 
-function testPropertyEscapes(regex, string, expression) {
-  if (!regex.test(string)) {
+function printCodePoint(codePoint) {
+  const hex = codePoint
+    .toString(16)
+    .toUpperCase()
+    .padStart(6, "0");
+  return `U+${hex}`;
+}
+
+function printStringCodePoints(string) {
+  const buf = [];
+  for (const symbol of string) {
+    const formatted = printCodePoint(symbol.codePointAt(0));
+    buf.push(formatted);
+  }
+  return buf.join(' ');
+}
+
+function testPropertyEscapes(regExp, string, expression) {
+  if (!regExp.test(string)) {
     for (const symbol of string) {
-      const hex = symbol
-        .codePointAt(0)
-        .toString(16)
-        .toUpperCase()
-        .padStart(6, "0");
+      printCodePoint(symbol.codePointAt(0));
       assert(
-        regex.test(symbol),
+        regExp.test(symbol),
         `\`${ expression }\` should match U+${ hex } (\`${ symbol }\`)`
+      );
+    }
+  }
+}
+
+function testPropertyOfStrings({ regExp, expression, matchStrings, nonMatchStrings }) {
+  const allStrings = matchStrings.join('');
+  if (!regex.test(allStrings)) {
+    for (const string of matchStrings) {
+      assert(
+        regex.test(string),
+        `\`${ expression }\` should match ${ string } (U+${ printStringCodePoints(string) })`
+      );
+    }
+  }
+
+  const allNonMatchStrings = nonMatchStrings.join('');
+  if (regex.test(allNonMatchStrings)) {
+    for (const string of nonMatchStrings) {
+      assert(
+        !regex.test(string),
+        `\`${ expression }\` should not match ${ string } (U+${ printStringCodePoints(string) })`
       );
     }
   }

485ae013e8abf4d1318d003e2c5c7cd71277e30a
Mathias Bynens <mathias@qiwi.be>
2022-02-15 16:01:53 +0100

    Generate tests for properties of strings (#10)
    This patch generates exhaustive tests that verifies every supported property of strings matches every single string it needs to. We also generate some similar-looking butdifferent strings and ensure those aren’t matched.
    
    Proposal: https://github.com/tc39/proposal-regexp-set-notation

--- build.js
+++ build.js
@@ -1,24 +1,46 @@
 const fs = require('fs');
+
 const jsesc = require('jsesc');
 
 const UNICODE_VERSION = require(
 	'regenerate-unicode-properties/unicode-version.js'
 );
 
-const _template = require('lodash.template');
-const TEST_TEMPLATE = fs.readFileSync('templates/test.template', 'utf8');
-const template = _template(TEST_TEMPLATE, {
-	'interpolate': /<%=([\s\S]+?)%>/g
-});
-
-const escape = (value) => {
+const stringify = (value) => {
 	return jsesc(value, {
-		'compact': false,
-		'numbers': 'hexadecimal',
-		'quotes': 'double',
-		'wrap': true,
+		compact: false,
+		quotes: 'double',
+		wrap: true,
+		es6: true,
+		indent: '  ',
+		indentLevel: 1,
 	});
 };
+
+const template = require('lodash.template');
+const templateOptions = {
+	interpolate: /<%=([\s\S]+?)%>/g,
+};
+const createCharacterPropertyTest = template(
+	fs.readFileSync('templates/character-property.template', 'utf8'),
+	templateOptions
+);
+const createPropertyOfStringsTest = template(
+	fs.readFileSync('templates/property-of-strings.template', 'utf8'),
+	templateOptions
+);
+const createPropertyOfStringsNegativePTest = template(
+	fs.readFileSync('templates/property-of-strings-negative-P.template', 'utf8'),
+	templateOptions
+);
+const createPropertyOfStringsNegativeCharacterClassTest = template(
+	fs.readFileSync('templates/property-of-strings-negative-CharacterClass.template', 'utf8'),
+	templateOptions
+);
+const createPropertyOfStringsNegativeUTest = template(
+	fs.readFileSync('templates/property-of-strings-negative-u.template', 'utf8'),
+	templateOptions
+);
 
 const regenerate = require('./regenerate.js');
 const UNICODE_SET = regenerate().addRange(0x0, 0x10FFFF);
@@ -34,12 +56,12 @@ const handleExpression = (property, value, symbols) => {
 	const nonMatchSymbols = mainExpression == 'Any' ?
 			'' :
 			UNICODE_SET.clone().remove(symbols).toTestCode();
-	const output = template({
-		'mainExpression': mainExpression,
-		'expressions': expressions,
-		'matchSymbols': matchSymbols,
-		'nonMatchSymbols': nonMatchSymbols,
-		'unicodeVersion': UNICODE_VERSION,
+	const output = createCharacterPropertyTest({
+		mainExpression: mainExpression,
+		expressions: expressions,
+		matchSymbols: matchSymbols,
+		nonMatchSymbols: nonMatchSymbols,
+		unicodeVersion: UNICODE_VERSION,
 	}).replace(/\n{3,}/g, '\n\n').trim() + '\n';
 	fs.writeFileSync(`output/generated/${ outputFile }.js`, output);
 };
@@ -53,4 +75,80 @@ for (const [property, values] of properties) {
 		);
 		handleExpression(property, value, set);
 	}
+}
+
+const createNonMatchingStrings = (matchingStrings) => {
+	const set = new Set(matchingStrings);
+	// 1. Find the longest `DESIRED_MAX_NUMBER_OF_STRINGS` strings.
+	const DESIRED_MAX_NUMBER_OF_STRINGS = 5;
+	const longestMatchingStrings = [...matchingStrings] // Avoid mutating.
+		.sort((a, b) => [...b].length - [...a].length)
+		//.filter(string => [...string].length > 1)
+		.slice(0, DESIRED_MAX_NUMBER_OF_STRINGS);
+	// 2. Generate possibly non-matching strings by removing code points
+	//		from the start, end, and code point index 1.
+	const nonMatchingStrings = [];
+	const addCandidate = (candidate) => {
+		// Verify the non-matching candidates are not in `matchingStrings`.
+		if (!set.has(candidate)) {
+			nonMatchingStrings.push(candidate);
+		}
+	};
+	for (const string of longestMatchingStrings) {
+		const symbols = [...string];
+		addCandidate(symbols.slice(1).join(''));
+		addCandidate(symbols.slice(0, -1).join(''));
+		if (symbols.length > 2) {
+			addCandidate(symbols[0] + symbols.slice(2).join(''));
+		}
+	}
+	return nonMatchingStrings;
+};
+
+const handlePropertyOfStrings = (property, strings) => {
+	console.log(`Handling \`\\p{${ property }}\`…`);
+	const outputFile = property;
+	const matchStrings = strings;
+	const nonMatchStrings = createNonMatchingStrings(matchStrings);
+
+	{
+		const output = createPropertyOfStringsTest({
+			property: property,
+			matchStrings: matchStrings,
+			nonMatchStrings: nonMatchStrings,
+			unicodeVersion: UNICODE_VERSION,
+			stringify: stringify,
+		}).trim() + '\n';
+		fs.writeFileSync(`output/generated/strings/${ outputFile }.js`, output);
+	}
+
+	{
+		const output = createPropertyOfStringsNegativePTest({
+			property: property,
+			unicodeVersion: UNICODE_VERSION,
+		}).trim() + '\n';
+		fs.writeFileSync(`output/generated/strings/${ outputFile }-negative-P.js`, output);
+	}
+
+	{
+		const output = createPropertyOfStringsNegativeCharacterClassTest({
+			property: property,
+			unicodeVersion: UNICODE_VERSION,
+		}).trim() + '\n';
+		fs.writeFileSync(`output/generated/strings/${ outputFile }-negative-CharacterClass.js`, output);
+	}
+
+	{
+		const output = createPropertyOfStringsNegativeUTest({
+			property: property,
+			unicodeVersion: UNICODE_VERSION,
+		}).trim() + '\n';
+		fs.writeFileSync(`output/generated/strings/${ outputFile }-negative-u.js`, output);
+	}
+};
+
+const propertiesOfStrings = require('@unicode/unicode-14.0.0').Sequence_Property;
+for (const property of propertiesOfStrings) {
+	const strings = require(`@unicode/unicode-14.0.0/Sequence_Property/${property}/index.js`);
+	handlePropertyOfStrings(property, strings);
 }
